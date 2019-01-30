2e613306ea8061969dffe75f7bc75d27cc0c6d8f
conconrob <connortprobertson@gmail.com>
2019-01-30 12:27:49 +0000

    DONE article tab

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,10 @@
-
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
-}
-
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
+const toggleMenu = () => {
+  // Toggle the "menu--open" class on your menu refence.
+  menu.classList.toggle('menu--open');
+};
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
